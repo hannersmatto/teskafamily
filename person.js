be84cbbc2ab0e126953380bbe4e5d76eca4dc96f
hannersmatto <brucematon@teskasoft.com>
2017-01-25 12:26:47 +1100

    Add files via upload

---
+++ person.js
@@ -0,0 +1,14 @@
+{
+    "name" : "Name1",
+        "age": 10,
+    "address" : {
+        "street" : "Street1",
+        "city" : "City1",
+        "state" : "State1",
+        "postcode" : "Post1"
+    },
+    "activities" : [
+        "act1",
+        "act2"
+    ]
+}
